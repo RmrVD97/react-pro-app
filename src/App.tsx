66120328f10ff312032915edb6549f298b0f97e2
RmrVD97 <ramiro.vela@hotmail.es>
2022-03-23 13:22:14 +0100

    Proyecto inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navigator } from './routes/Navigator';
+
+
+function App() {
+  return <>
+    <Navigator />
+  </>
+}
+
+export default App;
